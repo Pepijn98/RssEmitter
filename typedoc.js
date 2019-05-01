1684f67a7d427bb68efb4cd9c131e39d86a387b6
KurozeroPB <root@kurozeropb.info>
2019-05-01 20:29:15 +0200

    Fix empty package on npm

---
+++ typedoc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    src: [
+        "./lib",
+    ],
+    mode: "file",
+    includeDeclarations: true,
+    tsconfig: "tsconfig.json",
+    out: "./docs",
+    excludePrivate: true,
+    excludeProtected: true,
+    excludeExternals: true,
+    excludeNotExported: true,
+    readme: "README.md",
+    name: "RssEmitter",
+    ignoreCompilerErrors: true,
+    listInvalidSymbolLinks: true
+};
