051d1353cc971b442c925ad377beff50b2249467
Abhishek Datta <abdatta@iitk.ac.in>
2020-03-29 13:45:29 +0530

    Compile dist and update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,7 @@
 const { FeedEmitter } = require("../");
 
 const feeds = [
-    { url: "http://www.horriblesubs.info/rss.php?res=all", refresh: 20000, ignoreFirst: true },
+    { url: "http://www.horriblesubs.info/rss.php?res=all", refresh: 20000, eventName: "horriblesubs", ignoreFirst: true },
     { url: "https://hnrss.org/frontpage", refresh: 20000, ignoreFirst: true },
     { url: "https://github.com/reportapp/Api/commits/master.atom", refresh: 20000, ignoreFirst: true },
     { url: "https://github.com/kurozeropb/Jeanne/commits/master.atom", refresh: 20000, ignoreFirst: true },
@@ -17,6 +17,10 @@ emitter.on("feed:new", (config) => {
 
 emitter.on("item:new", (item) => {
     console.log(`New item: (${item.link})\n${item.title}\n${item.description}\n\n`);
+});
+
+emitter.on("item:new:horriblesubs", (item) => {
+    console.log(`New item from horriblesubs: (${item.link})\n${item.title}\n${item.description}\n\n`);
 });
 
 emitter.on("feed:error", (error) => {
